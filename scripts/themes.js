14551e8a0a3bdc56d57014497f1a539e79608b30
hanbo <hanbo.li@gmail.com>
2023-08-11 16:14:47 +0100

    logging

--- scripts/themes.js
+++ scripts/themes.js
@@ -8,6 +8,7 @@ function switchTheme(currentThemeIdx = themeIdx) {
     const newClassTheme = `theme-${currentThemeIdx + 1}`;
     let i = 0
     document.body.className = newClassTheme;
+    console.log('changing theme to', newClassTheme);
     document.body.className.replace(/(?:^|\s)theme-\d+(?!\S)/g , newClassTheme);
     themeIdx = (currentThemeIdx + 1) % maxThemes;
 }
