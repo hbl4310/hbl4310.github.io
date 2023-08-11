0429e3755dae8c2c811e90edd474cbc0fdb05fea
hanbo <hanbo.li@gmail.com>
2023-08-11 14:33:43 +0100

    dark mode preference

--- scripts/themes.js
+++ scripts/themes.js
@@ -5,11 +5,11 @@ let themeIdx = 0;
 const maxThemes = getComputedStyle(document.body).getPropertyValue("--num-themes");
 
 function switchTheme(currentThemeIdx = themeIdx) {
-    themeIdx = (currentThemeIdx + 1) % maxThemes;
-    const newClassTheme = `theme-${themeIdx + 1}`;
+    const newClassTheme = `theme-${currentThemeIdx + 1}`;
     let i = 0
     document.body.className = newClassTheme;
     document.body.className.replace(/(?:^|\s)theme-\d+(?!\S)/g , newClassTheme);
+    themeIdx = (currentThemeIdx + 1) % maxThemes;
 }
 
 function randomiseTheme() {
@@ -17,4 +17,19 @@ function randomiseTheme() {
     switchTheme(i);
 }
 
-window.addEventListener("DOMContentLoaded", randomiseTheme)
+function initTheme() {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        // dark mode defaults are theme-1 and theme-3
+        Math.random() > 0.5 ? switchTheme(0) : switchTheme(2);
+    } else {
+        randomiseTheme();
+    }
+}
+
+window.addEventListener("DOMContentLoaded", initTheme);
+
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+    // light mode defaults are theme-2 and theme-4
+    Math.random() > 0.5 ? switchTheme(1) : switchTheme(3);
+});
